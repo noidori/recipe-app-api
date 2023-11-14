bdd9857ef87ba013566b99a4051ae141d6782d26
no1dori <Idrisov201@protonmail.com>
2023-11-14 17:38:41 +0300

    fixing imports again

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -8,7 +8,7 @@ from django.urls import (
 
 from rest_framework.routers import DefaultRouter
 
-from app.recipe import views
+from recipe import views
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
